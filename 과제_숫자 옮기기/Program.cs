8c60cd4668a179fb5d589caf9db966bb325a79ee
siyeon1122si <ssiyeon1122@gmail.com>
2023-07-03 14:33:51 +0900

    숫자 옮기기

---
+++ 과제_숫자 옮기기/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _230616
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            One one = new One();
+            one.TurnOne();
+        }
+    }
+}
